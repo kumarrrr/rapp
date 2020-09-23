84048fc99abfc88283c13f0340c685ac4de87e54
Nirmal Kumar <Nirmal.N.Kumar2@shell.com>
2020-09-23 15:19:35 +0530

    Changed the h1 information

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const element = <h1>Hello World</h1>
+const element = <h1>Hello,Let's start towards react a wonderful UI library</h1>
 ReactDom.render(element,document.getElementById('root'));
+
+
